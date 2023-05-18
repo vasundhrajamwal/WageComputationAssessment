f7244fa2709fc1c93b06a43d84fedfeef06ad7b8
vasundhrajamwal <vasundhra.jamwal@gmail.com>
2023-05-18 18:43:29 +0530

    Welcome Message

---
+++ EmployeeWageWelcomeMessage.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.EmployeeWageComputationAssessment;
+public class EmployeeWageWelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Problem");
+    }
+}
